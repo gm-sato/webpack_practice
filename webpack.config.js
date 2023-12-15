1c4e4d94c678f9aafa08f46ded7c481fb57c1d84
r.sato <sato@dsnf.co.jp>
2023-12-15 15:56:33 +0900

    webpack5に更新

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,19 @@ module.exports = {
             ],
         }, {
             test: /\.(png|jpg)/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                    name: 'images/[name].[ext]',
-                }
-            }, ],
+            type: 'asset/resource',
+            generator: {
+                filename: 'images/[name][ext]',
+            },
+            use: [
+                //     {
+                //     loader: 'file-loader',
+                //     options: {
+                //         esModule: false,
+                //         name: 'images/[name].[ext]',
+                //     }
+                // }, 
+            ],
         }],
     },
     plugins: [
